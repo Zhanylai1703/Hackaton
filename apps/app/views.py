251ab1559c2f24f4aa00a435688c5790a15f6040
Zhanylai <kasymkulovajanylai@gmail.com>
2023-03-14 12:42:57 +0600

    commit

---
+++ apps/app/views.py
@@ -0,0 +1,37 @@
+
+
+# from rest_framework import generics
+
+# from .models import Flow, Direction, Student
+# from .serializers import FlowSerializer, DirectionSerializer, StudentSerializer
+
+
+# class FlowListAPIView(generics.ListAPIView):
+#     queryset = Flow.objects.all()
+#     serializer_class = FlowSerializer
+
+# class FlowCreateAPIView(generics.CreateAPIView):
+#     queryset = Flow.objects.all()
+#     serializer_class = FlowSerializer
+
+
+# class DirectionListAPIView(generics.ListAPIView):
+#     queryset = Direction.objects.all()
+#     serializer_class = DirectionSerializer
+
+# class DirectionCreateApiView(generics.CreateAPIView):
+#     queryset = Direction.objects.all()
+#     serializer_class = DirectionSerializer
+
+
+# class StudentListAPIView(generics.ListAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+
+# class StudentCreateAPIView(generics.CreateAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+
+# class StudentDestroyAPIView(generics.DestroyAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
